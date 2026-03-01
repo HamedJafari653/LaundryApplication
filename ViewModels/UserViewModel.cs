f153c1c575828a44eb00e39b344d2b4b62484c24
Hamed Jafari <hamed.jafari2003@gmail.com>
2026-03-01 19:18:20 +0100

    Started on UserViewmodel

--- ViewModels/UserViewModel.cs
+++ ViewModels/UserViewModel.cs
@@ -3,10 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using LaundryApplication.Models;
+using LaundryApplication.Services;
 
-namespace LaundryApplication.Controllers
+namespace LaundryApplication.ViewModels
 {
-    internal class UserController
+    internal class UserViewModel : BindableObject
     {
+        private readonly IUserService _user;
+        private User _currentUser;
+        private bool _IsBusy;
+
+        public User CurrentUser
+        {
+            get => _currentUser;
+            set { _currentUser = value; OnPropertyChanged();}
+        }
+        public bool IsBusy
+        {
+            get => _IsBusy;
+            set { _IsBusy = value; OnPropertyChanged(); }
+        }
+        public ICommand UpdateProfileCommand { get; }
+        public ICommand LogoutCommand { get; }
+
+        public UserViewModel(IUserService userService)
+        {
+            _userService = userService;
+
+            LoadUserData();
+            UpdateProfileCommand = new Command(async () => await OnUpdateProfile());
+        }
     }
 }
